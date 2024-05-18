93dffb289293185c5abc9f5bf046ab4bd1ea7f47
David Pittman <53283971+dopittman@users.noreply.github.com>
2024-05-18 16:15:06 -0400

    feat: add draw move

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
-win = Window(800, 600)
-point1 = Point(5,250)
-point2 = Point(40, 430)
-line1 = Line(point1, point2)
-point3 = Point(580,250)
-point4 = Point(450, 730)
-line2 = Line(point3, point4)
 
-win.draw_line(line1, "red")
-win.draw_line(line2, "red")
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
+    win.wait_for_close()
+
+
+main()
